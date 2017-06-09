d38f1945ffc674140f20aea4b411ef33de720456
David Diaz <david.diaz@zweicom.com>
2017-06-09 18:26:11 -0400

    modificacion en app/app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,17 @@
 // Importar el núcleo de Angular
 import {Component} from 'angular2/core';
+import {FooterComponent} from "./components/footer.component.ts";
  
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'my-app',
-    template: '<h1>Hola mundo con Angular 2 !! victorroblesweb.es</h1>'
+    //template: '<h1>Hola mundo con Angular 2 !! victorroblesweb.es</h1>',
+    templateUrl: 'app/view/inicio.html',
+    directives: [ FooterComponent ]
 })
  
 // Clase del componente donde iran los datos y funcionalidades
-export class AppComponent { }
+export class AppComponent { 
+
+
+}
