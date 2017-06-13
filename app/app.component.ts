91a9e4bfa08b1b6b8c27e8306409ce67607bd1cd
David Diaz <david.diaz@zweicom.com>
2017-06-13 15:28:17 -0400

    Se agregan nuevos componentes, al componente principal

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,15 @@
 // Importar el núcleo de Angular
 import {Component} from 'angular2/core';
-import {FooterComponent} from "./components/footer.component.ts";
+import {EncabezadoComponent} from "./components/encabezado.component";
+import {MenuComponent} from "./components/menu.component";
+import {FooterComponent} from "./components/footer.component";
  
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'my-app',
-    //template: '<h1>Hola mundo con Angular 2 !! victorroblesweb.es</h1>',
+    //template: '<h1>Hola mundo con Angular 2 !! </h1>',
     templateUrl: 'app/view/inicio.html',
-    directives: [ FooterComponent ]
+    directives: [ EncabezadoComponent, MenuComponent, FooterComponent ]
 })
  
 // Clase del componente donde iran los datos y funcionalidades
