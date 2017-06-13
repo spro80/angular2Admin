bacfb3636d18e655718f8b2fd7c0efba5d97e831
David Diaz <david.diaz@zweicom.com>
2017-06-13 12:29:24 -0400

    modificacion de footer

--- app/components/footer.component.ts
+++ app/components/footer.component.ts
@@ -3,14 +3,12 @@ import {Component} from 'angular2/core';
  
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
-    selector: 'footer',
-    templateUrl: 'app/view/footer.html'
+    selector: "footer",
+    templateUrl: "app/view/footer.html"
 })
  
 // Clase del componente donde iran los datos y funcionalidades
-export class FooterComponent { 
+export class FooterComponent{ 
 
-    public mensajeFooter:string = "Todos los derechos reservado 2017...";
-
-
+    public mensajeFooter:string = "Todos los derechos reservado 2017";
 }
