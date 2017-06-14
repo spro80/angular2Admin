1be9ba202c033f109accfb01ceac311c4bd53479
David Diaz <david.diaz@zweicom.com>
2017-06-14 16:36:40 -0400

    se incluye import y directives para categorias list component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,14 +2,16 @@
 import {Component} from 'angular2/core';
 import {EncabezadoComponent} from "./components/encabezado.component";
 import {MenuComponent} from "./components/menu.component";
+import {CategoriasListComponent} from "./components/categoriasList.component"
 import {FooterComponent} from "./components/footer.component";
+
  
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'my-app',
     //template: '<h1>Hola mundo con Angular 2 !! </h1>',
     templateUrl: 'app/view/inicio.html',
-    directives: [ EncabezadoComponent, MenuComponent, FooterComponent ]
+    directives: [ EncabezadoComponent, MenuComponent, FooterComponent, CategoriasListComponent ]
 })
  
 // Clase del componente donde iran los datos y funcionalidades
