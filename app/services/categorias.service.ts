40cda2fdf51a72f03fa32b8d3f4a1a63d7a5b918
David Diaz <david.diaz@zweicom.com>
2017-06-14 16:39:39 -0400

    se incluye categorias.service.ts

---
+++ app/services/categorias.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from 'angular2/core'
+
+
+@Injectable()
+export class CategoriasService{
+
+    getCategorias(){
+        return "RETORNANDO CATEGORIAS DESDE SERVICE...";
+    }
+
+}
+
