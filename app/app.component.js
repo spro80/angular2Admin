5a6ac2bb14334fe465f61ef6c66c706b2f232708
David Diaz <spro80@gmail.com>
2017-06-25 16:36:53 -0300

    se agregar constructor http en categorias.service

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', "./components/encabezado.component", "./components/menu.component", "./components/login.component", "./components/categoriasList.component", "./components/footer.component", "angular2/router"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,30 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, encabezado_component_1, menu_component_1, login_component_1, categoriasList_component_1, footer_component_1, router_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (encabezado_component_1_1) {
+                encabezado_component_1 = encabezado_component_1_1;
+            },
+            function (menu_component_1_1) {
+                menu_component_1 = menu_component_1_1;
+            },
+            function (login_component_1_1) {
+                login_component_1 = login_component_1_1;
+            },
+            function (categoriasList_component_1_1) {
+                categoriasList_component_1 = categoriasList_component_1_1;
+            },
+            function (footer_component_1_1) {
+                footer_component_1 = footer_component_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
             // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
@@ -25,8 +43,16 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>Hola mundo con Angular 2 !! victorroblesweb.es</h1>'
-                    }), 
+                        //template: '<h1>Hola mundo con Angular 2 !! </h1>',
+                        templateUrl: 'app/view/inicio.html',
+                        directives: [encabezado_component_1.EncabezadoComponent, menu_component_1.MenuComponent, footer_component_1.FooterComponent, categoriasList_component_1.CategoriasListComponent, login_component_1.LoginComponent,
+                            router_1.ROUTER_DIRECTIVES
+                        ]
+                    }),
+                    router_1.RouteConfig([
+                        { path: "/login", name: "Login", component: login_component_1.LoginComponent },
+                        { path: "/categorias", name: "Categorias", component: categoriasList_component_1.CategoriasListComponent }
+                    ]), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
