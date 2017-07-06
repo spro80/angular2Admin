66d2f49ecd5abe9d889b8da471c83352e75732fa
David Diaz <david.diaz@zweicom.com>
2017-07-06 15:38:41 -0400

    Se agregan import rxjs y model Categoria

--- app/services/categorias.service.ts
+++ app/services/categorias.service.ts
@@ -1,14 +1,17 @@
 import {Injectable} from 'angular2/core'
 import {Http, Response}           from 'angular2/http';
+import "rxjs/add/operator/map"
 import {Headers, RequestOptions}  from 'angular2/http';
 import {Observable}               from 'rxjs/Observable';
+import {Categoria} from '../model/categoria';
 
 @Injectable()
 export class CategoriasService{
 
-    constructor (private http: Http) {}
+    constructor (private _http: Http) {}
 	
     getCategorias(){
+        
         return "RETORNANDO CATEGORIAS 2 DESDE SERVICE con http...";
     }
 
