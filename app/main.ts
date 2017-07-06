fbebce5f4f874f58b94b38e5f73297e8bf42eca6
David Diaz <david.diaz@zweicom.com>
2017-07-06 16:06:38 -0400

    se agregan import HTTP_PROVIDERS

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router'
 import {AppComponent} from './app.component';
- 
-bootstrap(AppComponent , ROUTER_PROVIDERS);
+import {HTTP_PROVIDERS} from "angular2/http"
+bootstrap(AppComponent , ROUTER_PROVIDERS, HTTP_PROVIDERS);
